7381ff7b2f4aecb5c1ef78c52a71244f4ebba00e
Obenewa <ababioob75@gmail.com>
2023-05-05 13:49:10 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,17 +4,13 @@
 *
 * @n: the int to check
 *
-* Return: 1 and print + if n is greater than zero
-*
-* 0 and print 0 if n is zero
-*
-* -1 and print - if n is less than zero
+* Return: 1 for positive num, -1 for negative num or zero for anything else
 */
 int print_sign(int n)
 {
 if (n > 0)
 {
-_putchar('+');
+_putchar(43);
 return (1);
 }
 else if (n == 0)
@@ -24,7 +20,7 @@ return (0);
 }
 else if (n < 0)
 {
-_putchar('-');
+_putchar(45);
 return (-1);
 }
 }
