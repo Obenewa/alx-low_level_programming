ab1dbc8a1ae54b86c26f01578514fdbd2aea4312
Obenewa <ababioob75@gmail.com>
2023-05-04 20:02:10 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,7 @@
 #include "main.h"
 /**
 *print_alphabet_x10 - 10 times the alphabet
-*
-*Return: void
+**Return: void
 */
 void print_alphabet_x10(void)
 {
@@ -13,8 +12,8 @@ while (i <= 9)
 for (c = 'a'; c <= 'z'; c++)
 {
 _putchar(c);
+}
 _putchar('\n');
 i++;
-}
 }
 }
