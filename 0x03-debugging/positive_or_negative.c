241a8a4d36324de89e48ffb0609c012a008d3252
Obenewa <ababioob75@gmail.com>
2023-05-07 06:09:08 -0700

    Multiple mains

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -8,9 +8,9 @@
 void postitive_or_negative(int i)
 {
 if (i > 0)
-printf("%d is positive\n");
+printf("%d is positive\n", i);
 else if (i < 0)
-printf("%d is negative\n");
+printf("%d is negative\n", i);
 else
-printf("%d is zero\n");
+printf("%d is zero\n", i);
 }
