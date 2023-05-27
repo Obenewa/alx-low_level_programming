d17e80e81a7a4122000c339570e3b6ef38408daf
Obenewa <ababioob75@gmail.com>
2023-05-27 01:35:23 -0700

    create_static_lib.sh

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,9 +1,8 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
-char *_strcat(char *dest, char *src)
+int _atoi(char *s)
 {
-return strcat(dest, src);
+return atoi(s);
 }
